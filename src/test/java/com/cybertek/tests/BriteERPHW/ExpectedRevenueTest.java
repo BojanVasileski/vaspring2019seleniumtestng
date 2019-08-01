4cc5a079e6115213dbd40579e8ecb8ed68db9f5f
BojanVasileski <bojan.vasileski@gmail.com>
2019-08-01 19:26:32 -0400

    pushing

--- src/test/java/com/cybertek/tests/BriteERPHW/ExpectedRevenueTest.java
+++ src/test/java/com/cybertek/tests/BriteERPHW/ExpectedRevenueTest.java
@@ -9,19 +9,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class ExpectedRevenueTest extends TestBase {
-    @BeforeClass
+    @BeforeMethod
     public void setUp2(){
         Driver.get().get("http://54.148.96.210/web/login");
         BriteERPUtils.login(Driver.get(), ConfigurationReader.get("Manager5_username"),ConfigurationReader.get("Manager5_password"));
     }
 
-    @AfterClass
+    @AfterMethod
     public void tearDown(){
         Driver.closeDriver();
     }
@@ -63,6 +60,35 @@ public class ExpectedRevenueTest extends TestBase {
         String listRevenueValue = Driver.get().findElement(By.xpath("//td[@class='o_data_cell o_list_number'][contains(text(),'10.0')][1]")).getText();
 
         Assert.assertEquals(listRevenueValue,pivotRevenueValue);
+    }
+
+    @Test(priority = 2, description = "Verify total price is sum off all orportunity prices")
+    public void totalPivotTest(){
+        extentLogger = report.createTest("PivotAndListRevenueTest");
+
+        extentLogger.info("Selecting CRM module");
+        BriteERPUtils.selectModule("CRM");
+
+        CRMBritePage crmBritePage = new CRMBritePage();
+
+        extentLogger.info("Waiting for element to appear");
+        wait.until(ExpectedConditions.visibilityOf(crmBritePage.pivot));
+
+        extentLogger.info("Selecting Pivot view");
+        crmBritePage.pivot.click();
+
+        extentLogger.info("Setting Opportunity mode");
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//th[text()='Total']")));
+        Driver.get().findElement(By.xpath("//th[text()='Total']")).click();
+        wait.until(ExpectedConditions.visibilityOf(Driver.get().findElement(By.xpath("//a[@href='#'][contains(text(),'Opportunity')]"))));
+        Driver.get().findElement(By.xpath("//a[@href='#'][contains(text(),'Opportunity')]")).click();
+
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//td[@class='o_pivot_cell_value text-right'][contains(text(),'311.00')]")));
+        String exp = "311.00";
+        String acc = Driver.get().findElement(By.xpath("//td[@class='o_pivot_cell_value text-right'][contains(text(),'311.00')]")).getText();
+
+        Assert.assertEquals(exp,acc);
+
     }
 
 
